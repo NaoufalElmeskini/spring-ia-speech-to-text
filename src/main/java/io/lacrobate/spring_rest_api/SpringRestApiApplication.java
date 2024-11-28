b2e9c1d0cbe0ba85175117922b1544abac95f884
tintin <naoufal.elmeskini@gmail.com>
2024-11-28 11:34:33 +0100

    ajouter exemple wiremock avec feign

--- src/main/java/io/lacrobate/spring_rest_api/SpringRestApiApplication.java
+++ src/main/java/io/lacrobate/spring_rest_api/SpringRestApiApplication.java
@@ -2,10 +2,12 @@ package io.lacrobate.spring_rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/lacrobate/spring_rest_api/transcribe/TranscribeController.java
+++ src/main/java/io/lacrobate/spring_rest_api/transcribe/TranscribeController.java
@@ -1,10 +1,9 @@
-package io.lacrobate.spring_rest_api.product;
+package io.lacrobate.spring_rest_api.transcribe;
 
 import io.lacrobate.spring_rest_api.service.WhisperTutorial;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
